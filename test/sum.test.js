98290f1e4769243f46cfe5235054d2bf7c70fda0
Haider Ali <malikhaideraliraza@gmail.com>
2022-07-05 21:42:52 +0500

    updating sum feature 2

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,7 +1,7 @@
 const sum = require('../helper/sum');
 
 test('adds 1 + 2 to equal 5', () => {
-  expect(sum(1, 2)).toBe(5);
+  expect(sum(1, 2)).toBe(4);
 });
 
 test('adds 2 + 2 to equal 6', () => {
